d37e326c0bd2b0c528a3a469987595a91e02a8b4
postforty <75767073+postforty@users.noreply.github.com>
2022-06-09 00:23:27 +0900

    [UPDATE] 프로필 네브바 작업

--- client/src/layouts/MyPage.vue
+++ client/src/layouts/MyPage.vue
@@ -1,82 +1,45 @@
 <template>
   <div class="q-pa-md">
-    <!-- <q-layout
-      view="lHh Lpr lff"
-      container
-      style="height: 400px"
-      class="shadow-2 rounded-borders"
-    > -->
-    <!-- <q-header elevated class="bg-cyan-8">
-        <q-toolbar>
-          <q-toolbar-title>Header</q-toolbar-title>
-          <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
-        </q-toolbar>
-      </q-header> -->
-
-    <q-drawer v-model="drawer" show-if-above :width="200" :breakpoint="400">
-      <!-- <q-drawer v-model="drawer" show-if-above :width="200" :breakpoint="400"> -->
-      <q-scroll-area
-        style="
-          height: calc(100% - 150px);
-          margin-top: 150px;
-          border-right: 1px solid #ddd;
-        "
-      >
-        <q-list padding>
+    <q-drawer v-model="drawer" show-if-above :width="220" :breakpoint="400">
+      <div class="q-pa-sm">
+        <q-list dense bordered padding class="rounded-borders">
+          <q-item class="text-weight-bold">개발자의품격 님</q-item>
+          <q-item>abcd@gmail.com</q-item>
+        </q-list>
+      </div>
+      <div class="q-pa-sm">
+        <q-list dense bordered padding class="rounded-borders">
+          <q-item class="text-weight-bold">내 정보</q-item>
           <q-item clickable v-ripple @click="userProfile()">
-            <q-item-section avatar>
-              <q-icon name="inbox" />
-            </q-item-section>
-
             <q-item-section> 프로필 정보 </q-item-section>
           </q-item>
 
-          <q-item clickable v-ripple @click="joinBabsang()">
-            <q-item-section avatar>
-              <q-icon name="send" />
-            </q-item-section>
+          <q-item clickable v-ripple @click="userProfile()">
+            <q-item-section> 식사 매너 점수 </q-item-section>
+          </q-item>
 
+          <q-item style="margin-top: 20px" class="text-weight-bold"
+            >밥상 정보</q-item
+          >
+          <q-item clickable v-ripple @click="joinBabsang()">
             <q-item-section> 참여한 밥상 </q-item-section>
           </q-item>
 
           <!-- <q-item active clickable v-ripple @click="wishBabsang()"> -->
           <q-item clickable v-ripple @click="wishBabsang()">
-            <q-item-section avatar>
-              <q-icon name="star" />
-            </q-item-section>
-
             <q-item-section> 찜한 밥상 </q-item-section>
           </q-item>
 
-          <q-item clickable v-ripple @click="message()">
-            <q-item-section avatar>
-              <!-- <q-icon name="drafts" /> -->
-              <i
-                class="q-icon notranslate material-icons"
-                aria-hidden="true"
-                role="presentation"
-                >chat</i
-              >
-            </q-item-section>
-
-            <q-item-section> 메시지함 </q-item-section>
+          <q-item
+            style="margin-top: 20px"
+            clickable
+            v-ripple
+            @click="message()"
+          >
+            <q-item-section class="text-weight-bold"> 메시지함 </q-item-section>
           </q-item>
         </q-list>
-      </q-scroll-area>
-
-      <q-img
-        class="absolute-top"
-        src="https://cdn.quasar.dev/img/material.png"
-        style="height: 150px"
-      >
-        <div class="absolute-bottom bg-transparent">
-          <q-avatar size="56px" class="q-mb-sm">
-            <img src="../assets/img/profile.png" />
-          </q-avatar>
-          <div class="text-weight-bold">개발자의품격 님</div>
-          <div>abcd@gmail.com</div>
-        </div>
-      </q-img>
+      </div>
     </q-drawer>
     <q-page-container>
       <q-page padding>
--- client/src/pages/UserProfile.vue
+++ client/src/pages/UserProfile.vue
@@ -1,13 +1,27 @@
 <template>
   <q-page class="flex flex-center">
-   <h2>5 UserProfile</h2>
+    <h2>5 UserProfile</h2>
+    <div class="q-pa-md" style="max-width: 300px">
+      <q-input v-model="text" filled type="textarea" />
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'UserProfile'
-})
+  name: "UserProfile",
+  components: {},
+  data() {
+    return {
+      text: "",
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+});
 </script>
