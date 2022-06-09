77bf38aefa6fc0c8110957e46762b9ffc6e4254f
postforty <75767073+postforty@users.noreply.github.com>
2022-06-09 14:43:08 +0900

    [UPDATE] 프로필 정보 작업

--- client/src/pages/UserProfile.vue
+++ client/src/pages/UserProfile.vue
@@ -1,62 +1,51 @@
-'
 <template>
-  <q-page class="flex flex-center">
-    <table class="center">
-      <tbody>
-        <tr>
-          <td></td>
-          <td></td>
-          <td>
-            <q-item round flat>
-              <q-avatar size="200px">
-                <img src="../assets/img/profile.png" />
-              </q-avatar>
-            </q-item>
-          </td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td>
-            <q-item class="text-weight-bold">{{ profile }}</q-item>
-          </td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td>
-            <q-btn rounded color="primary" size="xs" label="수정" />
-          </td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr></tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
-    <div class="q-pa-md" style="max-width: 300px">
-      <q-input v-model="text" filled type="textarea" />
+  <q-page class="flex flex-top">
+    <div dense bordered padding class="rounded-borders">
+      <div class="col-12 flex flex-center q-mb-lg">
+        <q-avatar size="200px">
+          <img src="../assets/img/profile.png" />
+        </q-avatar>
+      </div>
+      <div class="col-12 column items-center">
+        <div class="q-pb-lg text-center">
+          <span class="text-bold text-no-wrap">{{ profile }}</span>
+        </div>
+        <div class="q-gutter-sm row items-center">
+          <q-btn outline rounded color="grey-7" size="xs" label="수정" />
+        </div>
+      </div>
+      <div class="col-12 column items-center">
+        <div class="q-pb-lg">
+          <table>
+            <tbody>
+              <tr>
+                <td style="height: 30px; width: 100px; text-align: center">
+                  <span class="text-bold text-no-wrap">성별</span>
+                </td>
+                <td>{{ gender }}</td>
+              </tr>
+              <tr>
+                <td style="height: 30px; width: 100px; text-align: center">
+                  <span class="text-bold text-no-wrap">식사 매너</span>
+                </td>
+                <td>{{ mannerScore }}</td>
+              </tr>
+              <tr>
+                <td style="height: 30px; width: 100px; text-align: center">
+                  <span class="text-bold text-no-wrap">자기 소개</span>
+                </td>
+              </tr>
+              <tr>
+                <td colspan="2">
+                  <div class="q-pa-md" style="width: 500px">
+                    <q-input v-model="text" filled autogrow type="textarea" />
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </q-page>
 </template>
@@ -71,6 +60,8 @@ export default defineComponent({
     return {
       profile: "개발자의 품격",
       text: '개발자의 품격 4기 2팀에서 구현 중인 혼밥 매칭 서비스 "겸상"입니다.',
+      gender: "남자",
+      mannerScore: "★★★★☆",
     };
   },
   setup() {},
@@ -85,17 +76,9 @@ export default defineComponent({
   width: 100%;
 }
 
-tr,
-td {
-  text-align: center;
-  border: 1px solid #ddd;
-}
-
 table {
   border-collapse: collapse;
-  /* border-spacing: 0; */
   width: 100%;
-  border: 1px solid #ddd;
   margin-left: auto;
   margin-right: auto;
 }
