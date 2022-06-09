ec5f2477815352daffdf72ffe2abe95e945fec18
postforty <75767073+postforty@users.noreply.github.com>
2022-06-09 23:53:40 +0900

    [UPDATE] 4번 숟갈 선택 화면 작업

--- client/src/layouts/MyPage.vue
+++ client/src/layouts/MyPage.vue
@@ -1,53 +1,57 @@
 <template>
-  <div class="q-pa-none">
-    <q-drawer v-model="drawer" show-if-above :width="220" :breakpoint="400">
-      <div class="q-ml-md q-my-md">
-        <q-list dense bordered padding class="rounded-borders">
-          <q-item class="text-weight-bold">{{ profile }} 님</q-item>
-          <q-item>{{ email }}</q-item>
-        </q-list>
-      </div>
-      <div class="q-ml-md q-my-md">
-        <q-list dense bordered padding class="rounded-borders">
-          <q-item class="text-weight-bold">내 정보</q-item>
-          <q-item clickable v-ripple @click="userProfile()">
-            <q-item-section> 프로필 정보 </q-item-section>
-          </q-item>
+  <q-page class="flex flex-center">
+    <div class="q-pa-none">
+      <q-drawer v-model="drawer" show-if-above :width="220" :breakpoint="400">
+        <div class="q-ml-md q-my-md">
+          <q-list dense bordered padding class="rounded-borders">
+            <q-item class="text-weight-bold">{{ profile }} 님</q-item>
+            <q-item>{{ email }}</q-item>
+          </q-list>
+        </div>
+        <div class="q-ml-md q-my-md">
+          <q-list dense bordered padding class="rounded-borders">
+            <q-item class="text-weight-bold">내 정보</q-item>
+            <q-item clickable v-ripple @click="userProfile()">
+              <q-item-section> 프로필 정보 </q-item-section>
+            </q-item>
 
-          <q-item clickable v-ripple @click="userProfile()">
-            <q-item-section> 식사 매너 점수 </q-item-section>
-          </q-item>
+            <q-item clickable v-ripple @click="userProfile()">
+              <q-item-section> 식사 매너 점수 </q-item-section>
+            </q-item>
 
-          <q-item style="margin-top: 20px" class="text-weight-bold"
-            >밥상 정보</q-item
-          >
-          <q-item clickable v-ripple @click="joinBabsang()">
-            <q-item-section> 참여한 밥상 </q-item-section>
-          </q-item>
+            <q-item style="margin-top: 20px" class="text-weight-bold"
+              >밥상 정보</q-item
+            >
+            <q-item clickable v-ripple @click="joinBabsang()">
+              <q-item-section> 참여한 밥상 </q-item-section>
+            </q-item>
 
-          <!-- <q-item active clickable v-ripple @click="wishBabsang()"> -->
-          <q-item clickable v-ripple @click="wishBabsang()">
-            <q-item-section> 찜한 밥상 </q-item-section>
-          </q-item>
+            <!-- <q-item active clickable v-ripple @click="wishBabsang()"> -->
+            <q-item clickable v-ripple @click="wishBabsang()">
+              <q-item-section> 찜한 밥상 </q-item-section>
+            </q-item>
 
-          <q-item
-            style="margin-top: 20px"
-            clickable
-            v-ripple
-            @click="message()"
-          >
-            <q-item-section class="text-weight-bold"> 메시지함 </q-item-section>
-          </q-item>
-        </q-list>
-      </div>
-    </q-drawer>
+            <q-item
+              style="margin-top: 20px"
+              clickable
+              v-ripple
+              @click="message()"
+            >
+              <q-item-section class="text-weight-bold">
+                메시지함
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
+      </q-drawer>
 
-    <!-- <q-page-container> -->
-    <q-page padding>
-      <router-view />
-    </q-page>
-    <!-- </q-page-container> -->
-  </div>
+      <!-- <q-page-container> -->
+      <q-page padding>
+        <router-view />
+      </q-page>
+      <!-- </q-page-container> -->
+    </div>
+  </q-page>
 </template>
 <script>
 import { ref } from "vue";
