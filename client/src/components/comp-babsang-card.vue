c93e3386324d27813b1fc7b5d6a81769d1333abc
seyoungjoy <diddpwl80@gmail.com>
2022-06-11 12:35:01 +0900

    [UPDATE] 밥상 컴포넌트 생성

---
+++ client/src/components/comp-babsang-card.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="">
+    <q-card class="my-card">
+      <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+
+      <q-card-section>
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">제주 최강 햄버거</div>
+        </div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">김민수</div>
+        <div class="text-caption text-grey">06.11~06.14</div>
+      </q-card-section>
+      <q-separator />
+
+      <q-card-actions>
+        <!--              <q-btn flat round icon="event" />-->
+        <q-btn
+          rounded
+          color="white"
+          text-color="black"
+          style="width: 60px"
+        >
+          모집중
+        </q-btn>
+        <q-btn
+          rounded
+          color="white"
+          text-color="black"
+          style="width: 60px"
+        >
+          혼성
+        </q-btn>
+        <q-btn
+          rounded
+          color="white"
+          text-color="black"
+          style="width: 60px"
+        >
+          1/4
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
+</template>
+<script>
+export default {
+  name:'comp-babsang-card',
+  components: {},
+  data() {
+    return {
+    };
+  },
+};
+</script>
