c74b48293ae8f9e8f7fb5053d645cd42c34f4e7a
seyoungjoy <diddpwl80@gmail.com>
2022-06-11 17:06:52 +0900

    [UPDATE] 밥상 생성폼 마크업

--- client/src/pages/CreateBabsang.vue
+++ client/src/pages/CreateBabsang.vue
@@ -1,13 +1,105 @@
 <template>
-  <q-page class="flex flex-center">
-    <h2>2 밥상 생성폼</h2>
+  <q-page style="width:800px; margin:0 auto;" class="">
+    <!-- **1 파일 업로드 -->
+    <div class="q-my-md">
+      <q-img
+
+        style="border-radius: 15px"
+        src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_1280.jpg  "
+        :ratio="16/9"
+      />
+    </div>
+    <!-- **2 밥상 정보 입력 -->
+    <div class="column">
+      <div class="q-pa-md" style="border : 1px solid gray; border-radius: 15px;">
+      <div class="col">
+        <q-input v-model="text" label="식당 이름" />
+      </div>
+      <div class="col">
+        <q-input color="purple-12" v-model="text" label="식당 위치 검색하기">
+          <template v-slot:prepend>
+            <q-icon name="pin_drop" />
+          </template>
+        </q-input>
+      </div>
+        <div class="col">
+          <q-input color="purple-12" v-model="text" label="식당 일시">
+            <template v-slot:prepend>
+              <q-icon name="calendar_month" />
+            </template>
+          </q-input>
+        </div>
+        <div class="col">
+          <q-input color="purple-12" v-model="text" label="모집 기간">
+            <template v-slot:prepend>
+              <q-icon name="schedule" />
+            </template>
+          </q-input>
+        </div>
+
+      <div class="col q-mt-xs" style="border-bottom:1px solid rgba(0, 0, 0, 0.24)">
+        <p style="font-size:25px;" class="q-ma-none">
+          <span>
+            <q-icon name="person"></q-icon>
+          </span>
+          <span class="q-field__label">성별 선택</span>
+        </p>
+        <div class="q-gutter-sm">
+          <q-radio v-model="shape" checked-icon="task_alt" unchecked-icon="panorama_fish_eye" val="mix" label="혼성" />
+          <q-radio v-model="shape" checked-icon="task_alt" unchecked-icon="panorama_fish_eye" val="man" label="남성" />
+          <q-radio v-model="shape" checked-icon="task_alt" unchecked-icon="panorama_fish_eye" val="woman" label="여성" />
+        </div>
+      </div>
+      <div class="col q-pb-md" style="border-bottom:1px solid rgba(0, 0, 0, 0.24)">
+        <p style="font-size:25px;" class="q-ma-none">
+          <span>
+            <q-icon name="groups"></q-icon>
+          </span>
+          <span class="q-field__label">모집 인원</span>
+        </p>
+        <q-input
+          v-model.number="model"
+          type="number"
+          filled
+          style="max-width: 200px"
+        />
+      </div>
+        <div class="col q-pb-md">
+          <p style="font-size:25px;" class="q-ma-none">
+          <span>
+            <q-icon name="groups"></q-icon>
+          </span>
+            <span class="q-field__label">밥상 소개하기</span>
+          </p>
+          <q-input
+            v-model="text"
+            filled
+            type="textarea"
+          />
+        </div>
+
+
+      </div>
+    </div>
+    <!-- **3 버튼 -->
+    <div class="row">
+      <q-btn>밥상 차리기</q-btn>
+      <q-btn>밥상 엎기</q-btn>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+// import { defineComponent } from "vue";
 
-export default defineComponent({
+export default {
   name: "CreateBabsang",
-});
+  data(){
+    return{
+      text:'',
+      shape:'',
+      model:4,
+    }
+  }
+};
 </script>
