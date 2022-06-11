9606c880f880c812410ef4183d6fe46d709ed06e
postforty <75767073+postforty@users.noreply.github.com>
2022-06-11 15:16:41 +0900

    [UPDATE] 화면 4

--- client/src/pages/SelectSpoon.vue
+++ client/src/pages/SelectSpoon.vue
@@ -1,48 +1,24 @@
 <template>
+  <div class="row text-h5 q-py-lg" style="width: 1100px; margin: 0 auto">
+    함께 식사할 숟갈을 선택해 주세요!
+  </div>
   <q-page class="row" style="width: 1100px; margin: 0 auto">
-    <div class="col">
-      <div class="text-h6 q-mb-sm">함께 식사할 숟갈을 선택해 주세요!</div>
-      <div>신청한 숟갈</div>
-      <div
-        class="q-card q-card--bordered q-card--flat no-shadow doc-example q-my-lg row"
-      >
-        <div class="q-pa-md row items-start q-gutter-md">
-          <q-card
-            class="my-card text-white"
-            style="
-              background: radial-gradient(circle, #35a2ff 0%, #014a88 100%);
-            "
-          >
-            <q-card-section>
-              <div class="text-h6">Our Changing Planet</div>
-              <div class="text-subtitle2">by John Doe</div>
-            </q-card-section>
-
-            <q-card-section class="q-pt-none">
-              {{ lorem }}
-            </q-card-section>
-          </q-card>
-
-          <q-card dark bordered class="bg-grey-9 my-card">
-            <q-card-section>
-              <div class="text-h6">Our Changing Planet</div>
-              <div class="text-subtitle2">by John Doe</div>
-            </q-card-section>
-
-            <q-separator dark inset />
-
-            <q-card-section>
-              {{ lorem }}
-            </q-card-section>
-          </q-card>
-        </div>
+    <div class="col-7">
+      <div class="row text-h6 q-pb-md">
+        <q-avatar size="23px">
+          <img src="../assets/img/icon-park-solid_spoon.png" />
+        </q-avatar>
+        신청한 숟갈
+      </div>
+      <div class="q-card q-card--bordered q-card--flat no-shadow q-pa-md row">
         <!-- 숟갈 카드 -->
         <div
           class="items-start col-6"
           :key="spoon.spoon_email"
           v-for="spoon in candidate"
         >
-          <q-card class="my-card" flat bordered>
+          <!-- <q-card class="my-card q-ma-xs" flat bordered> -->
+          <q-card class="my-card q-ma-xs" bordered>
             <q-item>
               <q-item-section avatar>
                 <q-avatar>
@@ -77,40 +53,96 @@
       </div>
     </div>
 
-    <div class="col">
-      <div>함께할 숟갈</div>
-      <div
-        class="q-card q-card--bordered q-card--flat no-shadow doc-example q-my-lg"
-      >
-        <div class="q-pa-md row items-start q-gutter-md">
-          <q-card
-            class="my-card text-white"
-            style="
-              background: radial-gradient(circle, #35a2ff 0%, #014a88 100%);
-            "
+    <div class="col-5">
+      <div class="q-ml-xl">
+        <div class="row text-h6 q-pb-md">
+          <q-avatar size="23px">
+            <img src="../assets/img/icon-park-solid_spoon.png" />
+          </q-avatar>
+          함께할 숟갈
+        </div>
+        <div
+          class="q-card q-card--bordered q-card--flat q-py-md q-px-xl no-shadow"
+        >
+          <div
+            class="items-start col-12 flex flex-center"
+            :key="spoon.spoon_email"
+            v-for="spoon in selected"
           >
-            <q-card-section>
-              <div class="text-h6">Our Changing Planet</div>
-              <div class="text-subtitle2">by John Doe</div>
-            </q-card-section>
+            <!-- <q-card class="my-card" flat>
+              <q-item>
+                <q-item-section avatar>
+                  <q-avatar>
+                    <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
+                  </q-avatar>
+                  <q-item-label>{{ spoon.nickname }}</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-card> -->
 
-            <q-card-section class="q-pt-none">
-              {{ lorem }}
-            </q-card-section>
-          </q-card>
+            <q-card class="my-card q-ma-xs" bordered>
+              <q-item>
+                <q-item-section avatar>
+                  <q-avatar>
+                    <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
+                  </q-avatar>
+                </q-item-section>
 
-          <q-card dark bordered class="bg-grey-9 my-card">
-            <q-card-section>
-              <div class="text-h6">Our Changing Planet</div>
-              <div class="text-subtitle2">by John Doe</div>
-            </q-card-section>
-
-            <q-separator dark inset />
-
-            <q-card-section>
-              {{ lorem }}
-            </q-card-section>
-          </q-card>
+                <q-item-section>
+                  <q-item-label>{{ spoon.nickname }}</q-item-label>
+                  <q-item-label caption
+                    >{{ spoon.gender }} {{ spoon.age_range }}</q-item-label
+                  >
+                </q-item-section>
+                <q-rating v-model="spoon.mannerScore" :max="5" size="20px" />
+              </q-item>
+              <q-card-section horizontal>
+                <q-card-section>
+                  <q-input
+                    v-model="spoon.dining_spoons_description"
+                    filled
+                    autogrow
+                    type="textarea"
+                    style="width: 220px"
+                  />
+                </q-card-section>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+        <div v-show="messageBoolean === true" class="q-pt-md">
+          <div class="row q-py-sm">함께할 숟갈들에게 메세지로 알려주세요!</div>
+          <q-input
+            v-model="message_description"
+            filled
+            autogrow
+            type="textarea"
+          />
+        </div>
+        <div align="center" class="q-pt-md">
+          <q-btn
+            v-show="messageBoolean === false"
+            unelevated
+            rounded
+            color="grey-5"
+            label="선정"
+            @click="messageShow()"
+          />
+          <q-btn
+            v-show="messageBoolean === true"
+            unelevated
+            rounded
+            color="grey-5"
+            label="확인"
+            @click="messageShow()"
+          />
+          <q-btn
+            class="q-ml-xl"
+            unelevated
+            rounded
+            color="grey-5"
+            label="취소"
+          />
         </div>
       </div>
     </div>
@@ -125,6 +157,8 @@ export default defineComponent({
   components: {},
   data() {
     return {
+      messageBoolean: false,
+      message_description: "",
       candidate: [
         {
           spoon_email: "spoon1@gmail.com",
@@ -223,12 +257,58 @@ export default defineComponent({
           selected_yn: "N",
         },
       ],
+      selected: [
+        {
+          spoon_email: "spoon1@gmail.com",
+          gender: "여성",
+          nickname: "숟갈1",
+          profile_image: "",
+          profile_description: "숟갈1 20대 여성입니다.",
+          age_range: "20대",
+          mannerScore: 3,
+          dining_spoons_description:
+            "저도 그 식당 가고싶었어요! 함께 먹고 싶어요 밥장님",
+          selected_yn: "N",
+        },
+        {
+          spoon_email: "spoon2@gmail.com",
+          gender: "여성",
+          nickname: "숟갈2",
+          profile_image: "",
+          profile_description: "숟갈2 20대 여성입니다.",
+          age_range: "20대",
+          mannerScore: 2,
+          dining_spoons_description:
+            "저도 그 식당 가고싶었어요! 함께 먹고 싶어요 밥장님",
+          selected_yn: "N",
+        },
+        {
+          spoon_email: "spoon3@gmail.com",
+          gender: "여성",
+          nickname: "숟갈3",
+          profile_image: "",
+          profile_description: "숟갈3 20대 여성입니다.",
+          age_range: "20대",
+          mannerScore: 4,
+          dining_spoons_description:
+            "저도 그 식당 가고싶었어요! 함께 먹고 싶어요 밥장님",
+          selected_yn: "N",
+        },
+      ],
     };
   },
   setup() {},
   created() {},
   mounted() {},
   unmounted() {},
-  methods: {},
+  methods: {
+    messageShow() {
+      if (this.messageBoolean === false) {
+        this.messageBoolean = true;
+      } else {
+        this.messageBoolean = false;
+      }
+    },
+  },
 });
 </script>
