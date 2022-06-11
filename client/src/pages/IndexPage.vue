24efc9cb9cfd7132b7423167a2c073bfbc5c1e56
seyoungjoy <diddpwl80@gmail.com>
2022-06-11 12:35:18 +0900

    [UPDATE] 컴포넌트 분리

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -96,192 +96,16 @@
     <!-- 밥상 카드 row -->
     <div class="row bab-group q-col-gutter-md">
       <div class="col-md-3 col-xs-12 bab-card">
-        <div class="">
-          <q-card class="my-card">
-            <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
-
-            <q-card-section>
-              <div class="row no-wrap items-center">
-                <div class="col text-h6 ellipsis">제주 최강 햄버거</div>
-              </div>
-            </q-card-section>
-
-            <q-card-section class="q-pt-none">
-              <div class="text-subtitle1">김민수</div>
-              <div class="text-caption text-grey">06.11~06.14</div>
-            </q-card-section>
-            <q-separator />
-
-            <q-card-actions>
-              <!--              <q-btn flat round icon="event" />-->
-              <q-btn
-                rounded
-                color="white"
-                text-color="black"
-                style="width: 60px"
-              >
-                모집중
-              </q-btn>
-              <q-btn
-                rounded
-                color="white"
-                text-color="black"
-                style="width: 60px"
-              >
-                혼성
-              </q-btn>
-              <q-btn
-                rounded
-                color="white"
-                text-color="black"
-                style="width: 60px"
-              >
-                1/4
-              </q-btn>
-            </q-card-actions>
-          </q-card>
-        </div>
+        <BabsangCard></BabsangCard>
       </div>
       <div class="col-md-3 col-xs-12 bab-card">
-        <div class="">
-          <q-card class="my-card">
-            <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
-
-            <q-card-section>
-              <div class="row no-wrap items-center">
-                <div class="col text-h6 ellipsis">제주 최강 햄버거</div>
-              </div>
-            </q-card-section>
-
-            <q-card-section class="q-pt-none">
-              <div class="text-subtitle1">김민수</div>
-              <div class="text-caption text-grey">06.11~06.14</div>
-            </q-card-section>
-            <q-separator />
-
-            <q-card-actions>
-              <!--              <q-btn flat round icon="event" />-->
-              <q-btn
-                rounded
-                color="white"
-                text-color="black"
-                style="width: 60px"
-              >
-                모집중
-              </q-btn>
-              <q-btn
-                rounded
-                color="white"
-                text-color="black"
-                style="width: 60px"
-              >
-                혼성
-              </q-btn>
-              <q-btn
-                rounded
-                color="white"
-                text-color="black"
-                style="width: 60px"
-              >
-                1/4
-              </q-btn>
-            </q-card-actions>
-          </q-card>
-        </div>
+        <BabsangCard></BabsangCard>
       </div>
       <div class="col-md-3 col-xs-12 bab-card">
-        <div class="">
-          <q-card class="my-card">
-            <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
-
-            <q-card-section>
-              <div class="row no-wrap items-center">
-                <div class="col text-h6 ellipsis">제주 최강 햄버거</div>
-              </div>
-            </q-card-section>
-
-            <q-card-section class="q-pt-none">
-              <div class="text-subtitle1">김민수</div>
-              <div class="text-caption text-grey">06.11~06.14</div>
-            </q-card-section>
-            <q-separator />
-
-            <q-card-actions>
-              <!--              <q-btn flat round icon="event" />-->
-              <q-btn
-                rounded
-                color="white"
-                text-color="black"
-                style="width: 60px"
-              >
-                모집중
-              </q-btn>
-              <q-btn
-                rounded
-                color="white"
-                text-color="black"
-                style="width: 60px"
-              >
-                혼성
-              </q-btn>
-              <q-btn
-                rounded
-                color="white"
-                text-color="black"
-                style="width: 60px"
-              >
-                1/4
-              </q-btn>
-            </q-card-actions>
-          </q-card>
-        </div>
+        <BabsangCard></BabsangCard>
       </div>
       <div class="col-md-3 col-xs-12 bab-card">
-        <div class="">
-          <q-card class="my-card">
-            <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
-
-            <q-card-section>
-              <div class="row no-wrap items-center">
-                <div class="col text-h6 ellipsis">제주 최강 햄버거</div>
-              </div>
-            </q-card-section>
-
-            <q-card-section class="q-pt-none">
-              <div class="text-subtitle1">김민수</div>
-              <div class="text-caption text-grey">06.11~06.14</div>
-            </q-card-section>
-            <q-separator />
-
-            <q-card-actions>
-              <!--              <q-btn flat round icon="event" />-->
-              <q-btn
-                rounded
-                color="white"
-                text-color="black"
-                style="width: 60px"
-              >
-                모집중
-              </q-btn>
-              <q-btn
-                rounded
-                color="white"
-                text-color="black"
-                style="width: 60px"
-              >
-                혼성
-              </q-btn>
-              <q-btn
-                rounded
-                color="white"
-                text-color="black"
-                style="width: 60px"
-              >
-                1/4
-              </q-btn>
-            </q-card-actions>
-          </q-card>
-        </div>
+        <BabsangCard></BabsangCard>
       </div>
     </div>
   </q-page>
@@ -290,9 +114,13 @@
 <script>
 import { defineComponent } from "vue";
 import { ref } from "vue";
+import BabsangCard from "../components/comp-babsang-card";
 
 export default defineComponent({
   name: "IndexPage",
+  components: {
+    BabsangCard,
+  },
   setup() {
     return {
       text: ref(""),
