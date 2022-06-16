14c71fc008dd35212ca8b3b0fa63cef78424d0b3
seyoungjoy <diddpwl80@gmail.com>
2022-06-17 00:04:03 +0900

    [UPDATE] eslint/prettier 세팅

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'bootstrap';
 
-createApp(App).use(store).use(router).mount("#app");
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+createApp(App).use(store).use(router).mount('#app');
