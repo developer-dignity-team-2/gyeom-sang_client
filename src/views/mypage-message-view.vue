26be4f9d0a07a6d8462031827c5e9b52236eab50
(yeojin youn) <indiayeojin@gmail.com>
2022-06-21 08:16:06 +0900

    [UPDATE] 8 메세지상세 border수정

--- src/views/mypage-message-view.vue
+++ src/views/mypage-message-view.vue
@@ -1,112 +1,114 @@
 <template>
-	<div class="container">
+	<div class="container mb-5">
 		<div class="row my-4">
 			<div class="col-md-3 col-sm-12">
 				<CompUserProfile />
 			</div>
 			<!-- 메세지 내용 -->
 			<div class="col-md-9 col-sm-12">
-				<h3>메세지 내용</h3>
-				<!-- 밥상메세지 -->
-				<div class="card border-primary mb-3" style="max-width: 80rem">
-					<div class="card-body">
-						<div class="card-text" style="height: 8rem">
-							<h5>밥상이 차려졌습니다.</h5>
-							<h6>해당 밥상의 url로 들어와주세요!</h6>
-							<h6>url</h6>
+				<div class="col border rounded p-3">
+					<h3>메세지 내용</h3>
+					<!-- 밥상메세지 -->
+					<div class="card border-primary mb-3" style="max-width: 80rem">
+						<div class="card-body">
+							<div class="card-text" style="height: 8rem">
+								<h5>밥상이 차려졌습니다.</h5>
+								<h6>해당 밥상의 url로 들어와주세요!</h6>
+								<h6>url</h6>
+							</div>
 						</div>
 					</div>
-				</div>
-				<!-- 구분선 -->
-				<div class="col my-3">
-					<hr style="background-color: #999" />
-				</div>
-				<div class="row">
-					<!-- 밥상 -->
-					<div class="col-8 col-sm-8">
-						<div class="card mb-3">
-							<div class="card-body">
-								<div class="d-flex justify-content-between">
-									<h5 class="card-title">밥상제목</h5>
-									<!-- 밥상 상세보기 버튼(해당 밥상으로 이동) -->
-									<button class="btn btn-primary" type="button">
-										상세보기
-									</button>
-								</div>
-							</div>
-							<img
-								src="https://cdn.pixabay.com/photo/2016/09/23/23/23/restaurant-1690696_1280.jpg"
-								alt="food1"
-								class="d-block user-select-none"
-								width="100%"
-								height="200"
-							/>
-							<!-- 소개내용 -->
-							<div class="card-body">
-								<p class="card-text">
-									안녕하세요 같이 제주도 연돈가실분 구해요
-								</p>
-							</div>
-							<!-- 밥상 정보(일시,장소,혼성여부) -->
-							<ul class="list-group list-group-flush">
-								<li class="list-group-item text-muted">식사 일시</li>
-								<li class="list-group-item text-muted">식당 위치</li>
-								<li class="list-group-item text-muted">혼성</li>
-							</ul>
-						</div>
+					<!-- 구분선 -->
+					<div class="col my-3">
+						<hr style="background-color: #999" />
 					</div>
-					<!-- 밥장프로필 -->
-					<div class="col-4 col-sm-4">
-						<div class="col border rounded p-3">
-							<div class="d-flex align-items-center">
-								<div class="profile me-3" style="width: 6rem">
-									<div class="img-wrap pf rounded-circle">
-										<img src="@/assets/img/exProfile.jpg" alt="프로필" />
+					<div class="row">
+						<!-- 밥상 -->
+						<div class="col-8 col-sm-8">
+							<div class="card mb-3">
+								<div class="card-body">
+									<div class="d-flex justify-content-between">
+										<h5 class="card-title">밥상제목</h5>
+										<!-- 밥상 상세보기 버튼(해당 밥상으로 이동) -->
+										<button class="btn btn-primary" type="button">
+											상세보기
+										</button>
 									</div>
 								</div>
-								<ul>
-									<li class="mb-1"><strong>밥장</strong></li>
-									<li class="mb-1">김민수</li>
-									<li>
-										<font-awesome-icon
-											icon="fa-solid fa-star"
-											style="color: #ffd24c"
-										/>
-										<font-awesome-icon
-											icon="fa-solid fa-star"
-											style="color: #ffd24c"
-										/>
-										<font-awesome-icon
-											icon="fa-solid fa-star"
-											style="color: #ffd24c"
-										/>
-										<font-awesome-icon
-											icon="fa-solid fa-star"
-											style="color: #ffd24c"
-										/>
-										<font-awesome-icon
-											icon="fa-solid fa-star"
-											style="color: #ffd24c"
-										/>
-									</li>
+								<img
+									src="https://cdn.pixabay.com/photo/2016/09/23/23/23/restaurant-1690696_1280.jpg"
+									alt="food1"
+									class="d-block user-select-none"
+									width="100%"
+									height="200"
+								/>
+								<!-- 소개내용 -->
+								<div class="card-body">
+									<p class="card-text">
+										안녕하세요 같이 제주도 연돈가실분 구해요
+									</p>
+								</div>
+								<!-- 밥상 정보(일시,장소,혼성여부) -->
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item text-muted">식사 일시</li>
+									<li class="list-group-item text-muted">식당 위치</li>
+									<li class="list-group-item text-muted">혼성</li>
 								</ul>
 							</div>
-							<div class="p-3">
-								책임감있는 밥장이 될게요! 믿고 맡겨 주세요~!
+						</div>
+						<!-- 밥장프로필 -->
+						<div class="col-4 col-sm-4">
+							<div class="col border rounded p-3">
+								<div class="d-flex align-items-center">
+									<div class="profile me-3" style="width: 6rem">
+										<div class="img-wrap pf rounded-circle">
+											<img src="@/assets/img/exProfile.jpg" alt="프로필" />
+										</div>
+									</div>
+									<ul>
+										<li class="mb-1"><strong>밥장</strong></li>
+										<li class="mb-1">김민수</li>
+										<li>
+											<font-awesome-icon
+												icon="fa-solid fa-star"
+												style="color: #ffd24c"
+											/>
+											<font-awesome-icon
+												icon="fa-solid fa-star"
+												style="color: #ffd24c"
+											/>
+											<font-awesome-icon
+												icon="fa-solid fa-star"
+												style="color: #ffd24c"
+											/>
+											<font-awesome-icon
+												icon="fa-solid fa-star"
+												style="color: #ffd24c"
+											/>
+											<font-awesome-icon
+												icon="fa-solid fa-star"
+												style="color: #ffd24c"
+											/>
+										</li>
+									</ul>
+								</div>
+								<div class="p-3">
+									책임감있는 밥장이 될게요! 믿고 맡겨 주세요~!
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<!-- 목록 , 삭제( 불필요할 것같아서 일단 주석처리 )버튼 -->
-				<div id="wrapper">
-					<button
-						type="button"
-						class="btn btn-outline-primary"
-						@click="message()"
-					>
-						목록
-					</button>
-					<!-- <button type="button" class="btn btn-outline-primary">삭제</button> -->
+					<!-- 목록 , 삭제( 불필요할 것같아서 일단 주석처리 )버튼 -->
+					<div id="wrapper">
+						<button
+							type="button"
+							class="btn btn-outline-primary"
+							@click="message()"
+						>
+							목록
+						</button>
+						<!-- <button type="button" class="btn btn-outline-primary">삭제</button> -->
+					</div>
 				</div>
 			</div>
 		</div>
