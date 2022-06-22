bf70fd5d0522a1bd27dc0e124661f57192f09cc9
seyoungjoy <diddpwl80@gmail.com>
2022-06-22 09:58:23 +0900

    [UPDATE] 주석 삭제

--- src/store/modules/babsang.js
+++ src/store/modules/babsang.js
@@ -7,39 +7,24 @@ export default {
 
 	getters: {
 		getBabsangData(state) {
-			// console.log('gettter');
-			// console.log(state.babsangData);
 			return state.babsangData;
 		},
-		// getBabsangData: state => state.babsangData,
 	},
 
 	mutations: {
 		setBabsangData(state, payload) {
 			state.babsangData = payload;
-			// console.log('mutation');
 			console.log(payload);
-			// console.log(state.babsangData);
 		},
 	},
 
 	actions: {
 		loadBabsangData({ commit }) {
-			return axios.get('https://nicespoons.com/api/v1/babsang').then(res => {
+			axios.get('https://nicespoons.com/api/v1/babsang').then(res => {
 				const res_data = res.data.result;
-				// console.log(res_data);
 				commit('setBabsangData', res_data);
-				// if (payload.onLoad !== undefined)
-				// payload.onLoad(getters.getBabsangData);
 			});
 		},
-		// loadBabsangData(context) {
-		// 	axios.get('https://nicespoons.com/api/v1/babsang').then(res => {
-		// 		console.log(res.data.result);
-		// 		// context.commit('setBabsangData', res.data.result);
-		// 		context.state.babsangData = res.data.result;
-		// 	});
-		// },
 
 		upLoadBabsang(context, payload) {
 			axios.post('https://nicespoons.com/api/v1/babsang', {
