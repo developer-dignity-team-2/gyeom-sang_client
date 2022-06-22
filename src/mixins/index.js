6f77e7c5ea314edfe686d1071ce6b2b1fb1a9c3d
seyoungjoy <diddpwl80@gmail.com>
2022-06-23 07:02:54 +0900

    [UPDATE] add mixin module

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -15,5 +15,33 @@ export default {
 				})
 			).data;
 		},
+		async $post(url, data) {
+			return await axios.post(url, data).catch(e => {
+				console.log(e);
+			});
+		},
+		async $put(url, data) {
+			return await axios.put(url, data).catch(e => {
+				console.log(e);
+			});
+		},
+		async $delete(url) {
+			return await axios.delete(url).catch(e => {
+				console.log(e);
+			});
+		},
+		async $upload(url, file) {
+			const formData = new FormData();
+			formData.append('attachment', file);
+			return (
+				await axios
+					.post(url, formData, {
+						headers: { 'Content-Type': 'multipart/form-data' },
+					})
+					.catch(e => {
+						console.log(e);
+					})
+			).data;
+		},
 	},
 };
