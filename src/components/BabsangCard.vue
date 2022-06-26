c931d83a2fa22e04113a367f48731ed61f7c6e54
seyoungjoy <diddpwl80@gmail.com>
2022-06-26 17:42:02 +0900

    [UPDATE] 밥상 카드 스타일 수정

--- src/components/BabsangCard.vue
+++ src/components/BabsangCard.vue
@@ -1,22 +1,44 @@
 <template>
-	<div class="card rounded" style="overflow: hidden; cursor: pointer">
-		<img
-			:src="'https://nicespoons.com/static/images/' + itemData.dining_thumbnail"
-			class="card-img-center"
-			:alt="itemData.restaurant_name"
-		/>
-		<div class="favorite">
-			<font-awesome-icon
-				icon="fa-solid fa-star"
-				size="2xl"
-				style="color: #ffd24c"
+	<div class="card rounded" style="overflow: hidden">
+		<div class="img-wrap">
+			<img
+				:src="
+					'https://nicespoons.com/static/images/' + itemData.dining_thumbnail
+				"
+				class="card-img-center"
+				:alt="itemData.restaurant_name"
 			/>
+			<div class="favorite">
+				<i
+					class="bi bi-heart pt-3 pe-2"
+					style="font-size: 1.5rem; color: rgb(255 72 95)"
+				></i>
+				<!-- <font-awesome-icon
+					icon="fa-solid fa-star"
+					style="color: #ffd24c; font-size: 1.5rem; cursor: pointer"
+				/> -->
+			</div>
+			<div class="shadow"></div>
+			<div class="author">
+				<div class="author-image">
+					<img src="../assets/img/users/m2.png" alt="" />
+					<span>유승완</span>
+				</div>
+			</div>
 		</div>
-		<div class="card-body">
+		<div class="card-body" style="cursor: pointer; padding: 2.3rem 2rem">
 			<div>
-				<h5 class="card-title">{{ itemData.restaurant_name }}</h5>
-				<p class="card-text mb-1">{{ itemData.nickname }}</p>
-				<p class="card-text mb-2">{{ startDate() }} ~ {{ endDate() }}</p>
+				<h5 class="card-title" style="font-weight: bold">
+					{{ itemData.restaurant_name }}
+				</h5>
+				<!-- <p class="card-text mb-1">{{ itemData.nickname }}</p> -->
+				<p class="card-text mb-2">
+					<i class="bi bi-geo-alt"></i> 제주시 연관읍 행복로 27
+				</p>
+				<p class="card-text mb-4">
+					<i class="bi bi-calendar-check me-2"></i>{{ startDate() }} ~
+					{{ endDate() }}
+				</p>
 			</div>
 			<div>
 				<button type="button" class="btn btn-primary me-2">
@@ -79,13 +101,62 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.card {
+	border-radius: 1.8rem !important;
+	border: none;
+	box-shadow: 0 10px 30px rgb(35 38 45 / 14%);
+
+	.img-wrap {
+		position: relative;
+		overflow: hidden;
+		width: 100%;
+		height: 100%;
+		.author {
+			position: absolute;
+			bottom: 1rem;
+			left: 1rem;
+			.author-image {
+				> img {
+					width: 2.5rem;
+					height: 2.5rem;
+					border-radius: 50%;
+					margin-right: 0.7rem;
+				}
+				> span {
+					font-weight: bold;
+					color: rgb(255, 255, 255);
+				}
+			}
+		}
+		.shadow {
+			position: absolute;
+			width: 100%;
+			height: 80px;
+			left: 0;
+			bottom: 0;
+			background: linear-gradient(transparent, #000000e3);
+			opacity: 0.35;
+		}
+		img.card-img-center {
+			transition: all 0.4s ease;
+		}
+		&:hover {
+			img.card-img-center {
+				transform: scale(1.1);
+				transition: all 0.4s ease;
+			}
+		}
+	}
+}
+
 .favorite {
 	position: absolute;
 	top: 0.5rem;
 	right: 0.5rem;
 }
 .card-img-center {
-	max-height: 15rem;
+	width: 100%;
+	max-height: 16rem;
 	object-fit: cover;
 }
 </style>
