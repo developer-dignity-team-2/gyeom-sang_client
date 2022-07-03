6c6fe8d08bb2d7af8e51bdbaea6dbeca312ec637
postforty <75767073+postforty@users.noreply.github.com>
2022-07-03 16:27:38 +0900

    [ADD] 마이페이지 > 메시지함 페이징 기능

---
+++ src/components/pagination/GridPagination.vue
@@ -0,0 +1,57 @@
+<template>
+	<div v-if="items !== undefined">
+		<simple-grid
+			:headers="headers"
+			:items="items"
+			:sliceStart="sliceStart"
+			:sliceEnd="sliceEnd"
+			@click-buttons="handleClickButtons"
+		/>
+		<pagination-packing
+			:itemCount="items.length"
+			@change-page="changePage"
+			:items="items"
+		/>
+	</div>
+</template>
+<script>
+import SimpleGrid from '@/components/pagination/SimpleGrid.vue';
+import PaginationPacking from '@/components/pagination/PaginationPacking.vue';
+export default {
+	components: { SimpleGrid, PaginationPacking },
+	props: {
+		headers: {
+			type: Array,
+			default: function () {
+				return [];
+			},
+		},
+		items: {
+			type: Array,
+			default: function () {
+				return [];
+			},
+		},
+	},
+	data() {
+		return {
+			sliceStart: 0,
+			sliceEnd: 5,
+		};
+	},
+	setup() {},
+	created() {},
+	mounted() {},
+	unmounted() {},
+	methods: {
+		changePage(start, end) {
+			// console.log('changePage', start, end)
+			this.sliceStart = start;
+			this.sliceEnd = end;
+		},
+		handleClickButtons(method, id) {
+			this.$emit('clickButtons', method, id);
+		},
+	},
+};
+</script>
