bb1e61e30f791ca87ad8ae5db3240f844a47bc12
postforty <75767073+postforty@users.noreply.github.com>
2022-07-03 19:59:42 +0900

    [UPDATE] 마이페이지 > 메시지 함 페이징
    API 연결 후 받은 메시지, 보낸 메시지 구분 등 추가 작업 필요함

--- src/components/pagination/SimpleGrid.vue
+++ src/components/pagination/SimpleGrid.vue
@@ -3,13 +3,69 @@
 		<table class="table table-striped" v-if="items.length > 0">
 			<thead>
 				<tr>
-					<th style="text-align: center">#</th>
+					<th scope="col" style="text-align: center">
+						<input
+							class="form-check-input"
+							type="checkbox"
+							v-model="checked_all"
+							@change="doSelectAll"
+						/>
+					</th>
+					<!-- <th style="text-align: center">#</th> -->
 					<th style="text-align: center" :key="th.key" v-for="th in headers">
 						{{ th.title }}
 					</th>
 				</tr>
 			</thead>
+			<!-- <tbody v-if="showMessage === 'R'"> -->
 			<tbody>
+				<tr
+					style="text-align: center"
+					:key="i"
+					v-for="(user, i) in formattedItems"
+				>
+					<td scope="row">
+						<input
+							class="form-check-input"
+							type="checkbox"
+							:value="user.email"
+							v-model="checked"
+							@change="doSelect"
+						/>
+					</td>
+					<!-- <td style="text-align: center">{{ i + 1 + sliceStart }}</td> -->
+					<td
+						v-for="th in headers"
+						:key="th.key"
+						class="text-left"
+						@click="messageView()"
+					>
+						{{ user[th.key] }}
+					</td>
+				</tr>
+			</tbody>
+			<!-- <tbody v-if="showMessage === 'S'">
+				<tr :key="i" v-for="(user, i) in sentMessage">
+					<td scope="row">
+						<input
+							class="form-check-input"
+							type="checkbox"
+							:value="user.email"
+							v-model="checked"
+							@change="doSelect"
+						/>
+					</td>
+					<td
+						v-for="th in Headers"
+						:key="th.key"
+						class="text-left"
+						@click="messageView()"
+					>
+						{{ user[th.key] }}
+					</td>
+				</tr>
+			</tbody> -->
+			<!-- <tbody>
 				<tr :key="i" v-for="(item, i) in formattedItems">
 					<td style="text-align: center">{{ i + 1 + sliceStart }}</td>
 					<td style="text-align: center" :key="th.key" v-for="th in headers">
@@ -24,7 +80,7 @@
 						/>
 					</td>
 				</tr>
-			</tbody>
+			</tbody> -->
 		</table>
 	</div>
 </template>
@@ -52,11 +108,13 @@ export default {
     sliceEnd: {
       type: Number,
       default: 100
-    }
+    },
   },
   data() {
     return {
-      // dataSortKey: ''
+      // dataSortKey: '',
+      checked_all: false,
+			checked: [],
     }
   },
   setup() {},
@@ -77,7 +135,19 @@ export default {
     },
     handleMethod(method, id) {
       this.$emit('clickButtons', method, id)
-    }
+    },
+    doSelect() {
+			console.log(this.checked);
+		},
+		doSelectAll() {
+			this.checked = [];
+			if (this.checked_all) {
+				for (let i in this.items) {
+					this.checked.push(this.items[i].email);
+				}
+			}
+      console.log(this.checked)
+		},
   }
 }
 </script>
