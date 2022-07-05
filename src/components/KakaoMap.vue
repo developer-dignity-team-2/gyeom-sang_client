2539a65e0fe1c70f1e26ea7f78fd3fd2628a6dc3
seyoungjoy <diddpwl80@gmail.com>
2022-07-06 08:11:16 +0900

    [UPDATE] 클릭시 해당 좌표 중심으로 이동

--- src/components/KakaoMap.vue
+++ src/components/KakaoMap.vue
@@ -6,6 +6,28 @@
 			<button @click="zoomOut()">ZOOM-</button>
 			<span id="maplevel"></span>
 		</div>
+		<div class="map-area">
+			<div class="searchbox">
+				<div>
+					<input
+						type="text"
+						v-model="keywordValue"
+						@keyup.enter="searchPlace"
+					/>
+				</div>
+				<div class="results">
+					<div
+						class="place"
+						v-for="rs in search.results"
+						:key="rs.id"
+						@click="showPlace(rs)"
+					>
+						<h3>{{ rs.place_name }}</h3>
+						<p>{{ rs.road_address_name }}</p>
+					</div>
+				</div>
+			</div>
+		</div>
 		<div id="map" style="width: 500px; height: 500px"></div>
 	</div>
 </template>
@@ -21,6 +43,16 @@ export default {
 			ps: null,
 			bounds: '',
 			mapInstance: '',
+			search: {
+				keyword: null,
+				pgn: null,
+				results: [],
+			},
+			keywordValue: '',
+			options: {
+				center: '',
+				level: '',
+			},
 		};
 	},
 	mounted() {
@@ -45,31 +77,31 @@ export default {
 	methods: {
 		initMap() {
 			const container = document.getElementById('map');
-			const options = {
+			this.options = {
 				center: new kakao.maps.LatLng(37.566826, 126.9786567),
 				level: 6,
 			};
-			this.mapInstance = new kakao.maps.Map(container, options);
+			this.mapInstance = new kakao.maps.Map(container, this.options);
 			console.log(this.mapInstance);
 			this.displayLevel();
-			// this.infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
-			// console.log(this.infowindow);
-			// const mapContainer = document.getElementById('map'); // 지도를 표시할 div
-			// const mapOption = {
-			// 	center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
-			// 	level: 3, // 지도의 확대 레벨
-			// };
-			// this.map = new kakao.maps.Map(mapContainer, mapOption);
-			// this.ps = new kakao.maps.services.Places();
-			// console.log(this.ps);
-			// this.ps.keywordSearch('이태원 맛집', this.placesSearchCB);
-			// const container = document.getElementById('map');
-			// const options = {
-			// 	center: new kakao.maps.LatLng(37.2429362, 131.8624647, 16),
-			// 	level: 5,
-			// };
-			// this.map = new kakao.maps.Map(container, options);
-			// console.log('로드 성공!');
+		},
+		searchPlace(e) {
+			this.keywordValue = e.target.value;
+			const keyword = e.target.value.trim();
+			if (keyword.length === 0) {
+				return;
+			}
+			const ps = new kakao.maps.services.Places();
+			ps.keywordSearch(keyword, (data, status, pgn) => {
+				this.search.keyword = keyword;
+				this.search.pgn = pgn;
+				this.search.results = data;
+			});
+			console.log(this.search);
+		},
+		showPlace(place) {
+			const moveLatLon = new kakao.maps.LatLng(place.y, place.x);
+			this.mapInstance.setCenter(moveLatLon);
 		},
 		zoomIn() {
 			const level = this.mapInstance.getLevel();
