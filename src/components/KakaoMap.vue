37f275a45abf9691d3d841a8a54261e4b6c8b9a5
seyoungjoy <diddpwl80@gmail.com>
2022-07-06 07:21:55 +0900

    [UPDATE] 확대 축소 추가

--- src/components/KakaoMap.vue
+++ src/components/KakaoMap.vue
@@ -1,6 +1,11 @@
 <template>
 	<div>
 		<h1>카카오맵 테스트</h1>
+		<div class="controll">
+			<button @click="zoomIn()">ZOOM+</button>
+			<button @click="zoomOut()">ZOOM-</button>
+			<span id="maplevel"></span>
+		</div>
 		<div id="map" style="width: 500px; height: 500px"></div>
 	</div>
 </template>
@@ -15,6 +20,7 @@ export default {
 			// 장소 검색 객체
 			ps: null,
 			bounds: '',
+			mapInstance: '',
 		};
 	},
 	mounted() {
@@ -25,7 +31,10 @@ export default {
 				process.env.VUE_APP_KAKAO_API_KEY;
 			document.head.appendChild(script);
 			/* global kakao */
+
 			script.addEventListener('load', () => {
+				console.log('----------kakao maps object----------');
+				console.log(kakao.maps);
 				kakao.maps.load(this.initMap);
 			});
 		} else {
@@ -35,22 +44,25 @@ export default {
 	unmounted() {},
 	methods: {
 		initMap() {
-			this.infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
-			console.log(this.infowindow);
-
-			const mapContainer = document.getElementById('map'); // 지도를 표시할 div
-			const mapOption = {
-				center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
-				level: 3, // 지도의 확대 레벨
+			const container = document.getElementById('map');
+			const options = {
+				center: new kakao.maps.LatLng(37.566826, 126.9786567),
+				level: 6,
 			};
-
-			this.map = new kakao.maps.Map(mapContainer, mapOption);
-
-			this.ps = new kakao.maps.services.Places();
-			console.log(this.ps);
-
-			this.ps.keywordSearch('이태원 맛집', this.placesSearchCB);
-
+			this.mapInstance = new kakao.maps.Map(container, options);
+			console.log(this.mapInstance);
+			this.displayLevel();
+			// this.infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+			// console.log(this.infowindow);
+			// const mapContainer = document.getElementById('map'); // 지도를 표시할 div
+			// const mapOption = {
+			// 	center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+			// 	level: 3, // 지도의 확대 레벨
+			// };
+			// this.map = new kakao.maps.Map(mapContainer, mapOption);
+			// this.ps = new kakao.maps.services.Places();
+			// console.log(this.ps);
+			// this.ps.keywordSearch('이태원 맛집', this.placesSearchCB);
 			// const container = document.getElementById('map');
 			// const options = {
 			// 	center: new kakao.maps.LatLng(37.2429362, 131.8624647, 16),
@@ -59,32 +71,47 @@ export default {
 			// this.map = new kakao.maps.Map(container, options);
 			// console.log('로드 성공!');
 		},
-		placesSearchCB(data, status) {
-			if (status === kakao.maps.services.status.ok) {
-				this.bounds = new kakao.maps.LatLngBounds();
-
-				for (let i = 0; i < data.length; i++) {
-					this.displayMarker(data[i]);
-					this.bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-				}
-				this.map.setBounds(this.bounds);
-			}
+		zoomIn() {
+			const level = this.mapInstance.getLevel();
+			this.mapInstance.setLevel(level - 1);
+			this.displayLevel();
 		},
-		displayMarker(place) {
-			const marker = new kakao.maps.Marker({
-				map: this.map,
-				position: new kakao.maps.LatLng(place.y, place.x),
-			});
-
-			marker.addEventListener('click', function () {
-				this.infowindow.setContent(
-					'<div style="padding:5px;font-size:12px;">' +
-						place.place_name +
-						'</div>',
-				);
-				this.infowindow.open(this.map, marker);
-			});
+		zoomOut() {
+			const level = this.mapInstance.getLevel();
+			this.mapInstance.setLevel(level + 1);
+			this.displayLevel();
 		},
+		displayLevel() {
+			const levelEl = document.getElementById('maplevel');
+			levelEl.innerHTML = '현재 지도 레벨은' + this.mapInstance.getLevel();
+		},
+
+		// placesSearchCB(data, status) {
+		// 	if (status === kakao.maps.services.status.ok) {
+		// 		this.bounds = new kakao.maps.LatLngBounds();
+
+		// 		for (let i = 0; i < data.length; i++) {
+		// 			this.displayMarker(data[i]);
+		// 			this.bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+		// 		}
+		// 		this.map.setBounds(this.bounds);
+		// 	}
+		// },
+		// displayMarker(place) {
+		// 	const marker = new kakao.maps.Marker({
+		// 		map: this.map,
+		// 		position: new kakao.maps.LatLng(place.y, place.x),
+		// 	});
+
+		// 	marker.addEventListener('click', function () {
+		// 		this.infowindow.setContent(
+		// 			'<div style="padding:5px;font-size:12px;">' +
+		// 				place.place_name +
+		// 				'</div>',
+		// 		);
+		// 		this.infowindow.open(this.map, marker);
+		// 	});
+		// },
 	},
 };
 </script>
