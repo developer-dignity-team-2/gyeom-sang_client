184e3815a4e72f60dfb107c01ceaf141109ffaf4
ding-co <pangoons@naver.com>
2022-07-07 18:28:22 +0900

    [DESIGN] SimpleGrid - cursor: pointer

--- src/components/pagination/SimpleGrid.vue
+++ src/components/pagination/SimpleGrid.vue
@@ -42,6 +42,7 @@
 					</td>
 					<!-- <td style="text-align: center">{{ i + 1 + sliceStart }}</td> -->
 					<td
+            style="cursor: pointer"
 						v-for="th in headers"
 						:key="th.key"
 						class="text-left"
