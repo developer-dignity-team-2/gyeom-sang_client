5c10a5183fbe2a29b5472e3bd55aa39da8855e4d
ding-co <pangoons@naver.com>
2022-07-09 11:58:11 +0900

    [REFACTOR] kakaoLogin component modularity

--- src/App.vue
+++ src/App.vue
@@ -42,13 +42,7 @@
 					</a>
 				</li> -->
 				<li>
-					<p
-						v-if="user.email === undefined"
-						@click="kakaoLogin"
-						style="display: inline; cursor: pointer; vertical-align: middle"
-					>
-						로그인
-					</p>
+					<KakaoLogin />
 				</li>
 				<li class="d-flex flex-column align-items-center mt-1">
 					<router-link to="/mypage/profile" v-if="user.email !== undefined">
@@ -72,62 +66,16 @@
 	<router-view />
 </template>
 <script>
-import axios from 'axios';
+import KakaoLogin from '@/components/login/KakaoLogin.vue';
 
 export default {
-	components: {},
+	components: { KakaoLogin },
 	computed: {
 		user() {
 			return this.$store.state.user.userData;
 		},
 	},
-	methods: {
-		/* 카카오톡 로그인 */
-		kakaoLogin() {
-			window.Kakao.Auth.login({
-				scope:
-					'openid, profile_nickname, profile_image, account_email, gender, age_range',
-				success: this.getProfile,
-			});
-		},
-		getProfile(authObj) {
-			console.log('---------------authObj data---------------');
-			console.log(authObj);
-			window.Kakao.API.request({
-				url: '/v2/user/me',
-				success: res => {
-					const kakao_account = res.kakao_account;
-					console.log('---------------kakao account---------------');
-					console.log(kakao_account);
-					this.login(kakao_account);
-					alert('로그인 성공!');
-				},
-			});
-		},
-		async login(kakao_account) {
-			const res = await axios.post('/auth/kakao/signup', {
-				param: {
-					email: kakao_account.email,
-					nickname: kakao_account.profile.nickname,
-					gender: kakao_account.gender,
-					profile_image: kakao_account.profile.profile_image_url,
-					age_range: kakao_account.age_range,
-				},
-			});
-			// res.data가 빈객체{}로 출력
-			// res.data.jwt는 undefined로 출력
-			console.log('---------------res.data---------------');
-			console.log(res.data);
-			localStorage.setItem('jwt', res.data.jwt);
-			console.log(res.data.jwt);
-
-			// vuex에 해당 유저정보를 저장
-			this.$store.commit('user/getUserData', kakao_account);
-			this.$store.commit('user/userCheck', true);
-			console.log('----------login store-----------');
-			console.log(this.$store.state.user.userData);
-		},
-	},
+	methods: {},
 };
 </script>
 
