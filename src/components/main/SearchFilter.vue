774c80612f6a75d5ddf3a036eb50f5797709568d
seyoungjoy <diddpwl80@gmail.com>
2022-07-17 12:54:50 +0900

    [UPDATE] 검색 필터 컴포넌트 추가

---
+++ src/components/main/SearchFilter.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="my-3">
+		<ul class="nav nav-pills">
+			<li class="nav-item dropdown">
+				<a
+					class="nav-link dropdown-toggle"
+					data-bs-toggle="dropdown"
+					href="#"
+					role="button"
+					aria-haspopup="true"
+					aria-expanded="false"
+					>지역</a
+				>
+				<div class="dropdown-menu" style="">
+					<a class="dropdown-item" href="#">서울</a>
+					<a class="dropdown-item" href="#">대전</a>
+					<a class="dropdown-item" href="#">대구</a>
+					<!-- <div class="dropdown-divider"></div> -->
+					<a class="dropdown-item" href="#">부산</a>
+				</div>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">기간</a>
+			</li>
+
+			<li class="nav-item dropdown">
+				<a
+					class="nav-link dropdown-toggle"
+					data-bs-toggle="dropdown"
+					href="#"
+					role="button"
+					aria-haspopup="true"
+					aria-expanded="false"
+					>성별</a
+				>
+				<div class="dropdown-menu" style="">
+					<a class="dropdown-item" href="#">혼성</a>
+					<a class="dropdown-item" href="#">남성</a>
+					<a class="dropdown-item" href="#">여성</a>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
--- src/views/main.vue
+++ src/views/main.vue
@@ -32,49 +32,9 @@
 				</div>
 			</div>
 		</div>
+		<!-- 검색 필터 -->
 		<div class="row">
-			<div class="my-3">
-				<ul class="nav nav-pills">
-					<li class="nav-item dropdown">
-						<a
-							class="nav-link dropdown-toggle"
-							data-bs-toggle="dropdown"
-							href="#"
-							role="button"
-							aria-haspopup="true"
-							aria-expanded="false"
-							>지역</a
-						>
-						<div class="dropdown-menu" style="">
-							<a class="dropdown-item" href="#">서울</a>
-							<a class="dropdown-item" href="#">대전</a>
-							<a class="dropdown-item" href="#">대구</a>
-							<!-- <div class="dropdown-divider"></div> -->
-							<a class="dropdown-item" href="#">부산</a>
-						</div>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">기간</a>
-					</li>
-
-					<li class="nav-item dropdown">
-						<a
-							class="nav-link dropdown-toggle"
-							data-bs-toggle="dropdown"
-							href="#"
-							role="button"
-							aria-haspopup="true"
-							aria-expanded="false"
-							>성별</a
-						>
-						<div class="dropdown-menu" style="">
-							<a class="dropdown-item" href="#">혼성</a>
-							<a class="dropdown-item" href="#">남성</a>
-							<a class="dropdown-item" href="#">여성</a>
-						</div>
-					</li>
-				</ul>
-			</div>
+			<SearchFilter></SearchFilter>
 		</div>
 		<!-- 밥상카드 -->
 		<div class="row">
@@ -85,11 +45,12 @@
 
 <script>
 import MainCardList from '@/components/main/MainCardList';
-
+import SearchFilter from '@/components/main/SearchFilter';
 export default {
 	name: 'Main',
 	components: {
 		MainCardList,
+		SearchFilter,
 	},
 	data() {
 		return {
