5b586dc80ba37f532a9568d1a7725d00a3da61bb
postforty <75767073+postforty@users.noreply.github.com>
2022-07-20 01:03:38 +0900

    [UPDATE] 데이트 피커 메인 컬러 적용

--- src/components/main/SearchFilter.vue
+++ src/components/main/SearchFilter.vue
@@ -120,6 +120,30 @@ export default {
 };
 </script>
 <style scoped lang="scss">
+::v-global(.dp__theme_light) {
+	--dp-background-color: #fff;
+	--dp-text-color: #212121;
+	--dp-hover-color: #f3f3f3;
+	--dp-hover-text-color: #212121;
+	--dp-hover-icon-color: #959595;
+	--dp-primary-color: #ffcb00;
+	--dp-primary-disabled-color: #6bacea;
+	--dp-primary-text-color: #f8f5f5;
+	--dp-secondary-color: #c0c4cc;
+	--dp-border-color: #ddd;
+	--dp-menu-border-color: #ddd;
+	--dp-border-color-hover: #aaaeb7;
+	--dp-disabled-color: #f6f6f6;
+	--dp-scroll-bar-background: #f3f3f3;
+	--dp-scroll-bar-color: #959595;
+	--dp-success-color: #ffcb00;
+	--dp-success-color-disabled: #a3d9b1;
+	--dp-icon-color: #959595;
+	--dp-danger-color: #ff6f60;
+	--dp-marker-color: #ff6f60;
+	--dp-tooltip-color: #fafafa;
+	--dp-disabled-color-text: #8e8e8e;
+}
 .nav {
 	align-items: center;
 	.nav-item {
