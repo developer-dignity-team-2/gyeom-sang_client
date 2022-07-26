2c44f8139f8e769e6345cbe08c35bfbfec1baf51
postforty <75767073+postforty@users.noreply.github.com>
2022-07-26 10:06:10 +0900

    Revert "[UPDATE] 메시지함 반응형 모바일 사이즈 작업중"
    This reverts commit f61eecec395ab1905c5c5cd2e931db3bab2a77ee.

--- src/components/pagination/SimpleGrid.vue
+++ src/components/pagination/SimpleGrid.vue
@@ -1,10 +1,7 @@
 <template>
 	<!-- 받은 메시지 -->
 	<div style="overflow: auto" v-if="$store.state.button.showMessage === 'R'">
-		<table
-			class="table table-hover"
-			v-if="items.length > 0 && window.width > 767"
-		>
+		<table class="table table-hover" v-if="items.length > 0">
 			<thead>
 				<tr>
 					<th scope="col" style="text-align: center">
@@ -59,55 +56,10 @@
 				</tr>
 			</tbody>
 		</table>
-		<div v-if="items.length > 0 && window.width <= 767">
-			<div
-				class="row"
-				style="text-align: center; cursor: pointer"
-				:key="i"
-				v-for="(user, i) in formattedItems"
-			>
-				<div class="col-1">
-					<input
-						class="form-check-input"
-						type="checkbox"
-						:value="user.id"
-						v-model="checked"
-						@change="doSelect"
-					/>
-				</div>
-				<div
-					:key="item.key"
-					v-for="item in items"
-					class="col-11"
-					:class="{ readMessage: user.read_check === 'Y' }"
-					@click="messageView(user.id)"
-				>
-					<span style="display: inline-block; width: 30%; text-align: left">{{
-						item.sender_nickname
-					}}</span>
-					<span style="display: inline-block; width: 70%; text-align: right">{{
-						item.create_date
-					}}</span>
-					<div
-						class="text-left"
-						style="
-							overflow: hidden;
-							text-overflow: ellipsis;
-							white-space: nowrap;
-						"
-					>
-						{{ item.message_description }}
-					</div>
-				</div>
-			</div>
-		</div>
 	</div>
 	<!-- 보낸 메시지 -->
 	<div style="overflow: auto" v-if="$store.state.button.showMessage === 'S'">
-		<table
-			class="table table-hover"
-			v-if="items.length > 0 && window.width > 767"
-		>
+		<table class="table table-hover" v-if="items.length > 0">
 			<thead>
 				<tr>
 					<th scope="col" style="text-align: center">
@@ -162,177 +114,127 @@
 				</tr>
 			</tbody>
 		</table>
-		<div v-if="items.length > 0 && window.width <= 767">
-			<div
-				class="row"
-				style="text-align: center; cursor: pointer"
-				:key="i"
-				v-for="(user, i) in formattedItems"
-			>
-				<div class="col-1">
-					<input
-						class="form-check-input"
-						type="checkbox"
-						:value="user.id"
-						v-model="checked"
-						@change="doSelect"
-					/>
-				</div>
-				<div
-					:key="item.key"
-					v-for="item in items"
-					class="col-11"
-					:class="{ readMessage: user.read_check === 'Y' }"
-					@click="messageView(user.id)"
-				>
-					<span style="display: inline-block; width: 30%; text-align: left">{{
-						item.receiver_nickname
-					}}</span>
-					<span style="display: inline-block; width: 70%; text-align: right">{{
-						item.create_date
-					}}</span>
-					<div
-						class="text-left"
-						style="
-							overflow: hidden;
-							text-overflow: ellipsis;
-							white-space: nowrap;
-						"
-					>
-						{{ item.message_description }}
-					</div>
-				</div>
-			</div>
-		</div>
 	</div>
 </template>
 <script>
+/* eslint-disable */
 export default {
-	components: {},
-	props: {
-		items: {
-			type: Array,
-			default: function () {
-				return [];
-			},
-		},
-		headers: {
-			type: Array,
-			default: function () {
-				return [];
-			},
-		},
-		sliceStart: {
-			type: Number,
-			default: 0,
-		},
-		sliceEnd: {
-			type: Number,
-			default: 100,
-		},
-	},
-	data() {
-		return {
-			// dataSortKey: '',
-			checked_all: false,
-			checked: [],
-			window: { width: 0, height: 0 },
-		};
-	},
-	setup() {},
-	created() {
-		window.addEventListener('resize', this.handleResize);
-		this.handleResize();
-	},
-	unmounted() {
-		window.removeEventListener('resize', this.handleResize);
-	},
-	computed: {
-		formattedItems: function () {
-			// console.log('grid: ', this.items.length)
-			return this.items.slice(this.sliceStart, this.sliceEnd);
-		},
-	},
-	mounted() {},
-	// unmounted() {},
-	methods: {
-		handleResize() {
-			this.window.width = window.innerWidth;
-			this.window.height = window.innerHeight;
-		},
-		changePage(start, end) {
-			// console.log('changePage', start, end);
-			// eslint-disable-next-line vue/no-mutating-props
-			this.sliceStart = start;
-			// eslint-disable-next-line vue/no-mutating-props
-			this.sliceEnd = end;
-		},
-		handleMethod(method, id) {
-			this.$emit('clickButtons', method, id);
-		},
-		doSelect() {
-			this.$store.commit('message/checkedMessage', this.checked);
-			console.log('Vuex Test : ', this.$store.state.message.checkedMessage);
-		},
-		doSelectAll() {
-			this.checked = [];
-			if (this.checked_all) {
-				for (let i in this.items) {
-					this.checked.push(this.items[i].id);
-				}
-			}
-			this.$store.commit('message/checkedMessage', this.checked);
-			console.log(
-				'vuex에 전체 선택 메시지 테스트 : ',
-				this.$store.state.message.checkedMessage,
-			);
-		},
-		// 메시지 읽음 처리
-		async putReadMessage(id) {
-			const loader = this.$loading.show({ canCancel: false });
+  components: {},
+  props: {
+    items: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+    headers: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+    sliceStart: {
+      type: Number,
+      default: 0,
+    },
+    sliceEnd: {
+      type: Number,
+      default: 100,
+    },
+  },
+  data() {
+    return {
+      // dataSortKey: '',
+      checked_all: false,
+      checked: [],
+    };
+  },
+  setup() {},
+  created() {},
+  computed: {
+    formattedItems: function () {
+      // console.log('grid: ', this.items.length)
+      return this.items.slice(this.sliceStart, this.sliceEnd);
+    },
+  },
+  mounted() {},
+  unmounted() {},
+  methods: {
+    changePage(start, end) {
+      // console.log('changePage', start, end)
+      this.sliceStart = start;
+      this.sliceEnd = end;
+    },
+    handleMethod(method, id) {
+      this.$emit('clickButtons', method, id);
+    },
+    doSelect() {
+      this.$store.commit('message/checkedMessage', this.checked);
+      console.log('Vuex Test : ', this.$store.state.message.checkedMessage);
+    },
+    doSelectAll() {
+      this.checked = [];
+      if (this.checked_all) {
+        for (let i in this.items) {
+          this.checked.push(this.items[i].id);
+        }
+      }
+      this.$store.commit('message/checkedMessage', this.checked);
+      console.log(
+        'vuex에 전체 선택 메시지 테스트 : ',
+        this.$store.state.message.checkedMessage
+      );
+    },
+    // 메시지 읽음 처리
+    async putReadMessage(id) {
+      const loader = this.$loading.show({ canCancel: false });
 
-			const readMessage = await this.$put(`/message/${id}`, {
-				param: {
-					// "message_description": "읽음 처리했습니다.",
-					read_check: 'Y',
-				},
-			});
+      const readMessage = await this.$put(
+        `/message/${id}`,
+        {
+          param: {
+            // "message_description": "읽음 처리했습니다.",
+            "read_check": "Y"
+          },
+        },
+      );
 
-			loader.hide();
-			console.log(readMessage);
-		},
-		// 메시지 상세보기 라우터
-		messageView(id) {
-			console.log('messageView : ', id);
-			this.$router.push({
-				name: 'MypageMessageView',
-				query: { messageId: id },
-			});
-			this.putReadMessage(id); // 메시지 읽음 처리
-		},
-	},
+	  loader.hide();
+      console.log(readMessage);
+    },
+    // 메시지 상세보기 라우터
+    messageView(id) {
+      console.log("messageView : ", id);
+      this.$router.push({
+        name: 'MypageMessageView',
+        query: { messageId: id },
+      });
+      this.putReadMessage(id); // 메시지 읽음 처리
+    },
+  },
 };
 </script>
 <style scoped lang="scss">
 // 메시지 말줄임 처리
 .nickname-width {
-	max-width: 120px;
+  max-width: 120px;
 }
 .description-width {
-	max-width: 320px;
+  max-width: 320px;
 }
 .location-width {
-	max-width: 160px;
+  max-width: 160px;
 }
 td {
-	//  min-width: 200px;
-	//  max-width: 300px;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	white-space: nowrap;
+  //  min-width: 200px;
+  //  max-width: 300px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
+
 // 메시지 읽음 처리
 .readMessage {
-	color: #cfcfcf;
+  color: #CFCFCF
 }
 </style>
